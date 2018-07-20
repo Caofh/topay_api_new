dce28023b08c508a98431f4ab7a97ff020df620d
caofanghui <caofanghui@vipkid.com.cn>
2018-07-20 18:08:52 +0800

    Update-添加后端图片压缩功能.

--- application/controllers/api/Common.php
+++ application/controllers/api/Common.php
@@ -41,18 +41,78 @@ class Common extends CI_Controller {
         //返回结果只能是布尔值
         $status=$this->upload->do_upload('file');
 
-        if($status) {
-            renderJson([
-                'status'=>true,
-                'info'=>$this->upload->data(), //获取上传信息
-                'target_path' => RESOURCE_URL.'/' . 'uploads' . '/' . $targetDir . $this->upload->data()['orig_name']
-            ]);
-        }else{
-            renderJson([
-                'status'=>false,
-                'info'=>$this->upload->display_errors()
-            ]);
+        // 压缩大于1M图片成缩略图
+        $data = array('upload_data' => $this->upload->data());
+        //"is_image":true,"image_width":169,"image_height":94,
+        $data['width'] = $data['upload_data']['image_width'];
+        $data['height'] = $data['upload_data']['image_height'];
+        $data['path'] = $data['upload_data']['full_path'];
+
+//        var_dump($data['upload_data']['file_size']);
+//        exit;
+        // 超过1M的图片进行压缩，压缩成width：1500；height：1500的图片，并上传到阿里云上
+        if ($data['upload_data']['file_size'] > 1 * 1024) {
+            $this->load->library("image_lib");//载入图像处理类库
+
+            $config_big_thumb['image_library'] = 'gd2';//gd2图库
+            $config_big_thumb['source_image'] = $data['path'];//原图
+            $config_big_thumb['new_image'] = "./uploads" . '/' . $data['upload_data']['file_name'];//大缩略图
+            $config_big_thumb['quality'] = "90%";//大缩略图
+            $config_big_thumb['create_thumb'] = true;//是否创建缩略图
+            $config_big_thumb['maintain_ratio'] = true;
+            $config_big_thumb['width'] = 1500;//缩略图宽度
+            $config_big_thumb['height'] = 1500;//缩略图的高度
+            $config_big_thumb['thumb_marker'] = "_1500_1500";//缩略图名字后加上 "_300_300",可以代表是一个300*300的缩略图
+
+            $this->image_lib->initialize($config_big_thumb);
+            $this->image_lib->resize();//生成big缩略图
+
+            // 判断是否压缩图片（生成缩略图）成功
+            if (!$this->image_lib->resize())
+            {
+                $error = [
+                    'original' => [
+                        'status' => 'fail',
+                        'msg' => $this->image_lib->display_errors(),
+                    ],
+                ];
+
+                return renderJson($error);
+
+            }else{
+
+                if($status) {
+                    renderJson([
+                        'status'=>true,
+                        'info'=>$this->upload->data(), //获取上传信息
+                        'target_path' => RESOURCE_URL.'/' . 'uploads' . '/' . $targetDir . $this->upload->data()['orig_name']
+                    ]);
+                }else{
+                    renderJson([
+                        'status'=>false,
+                        'info'=>$this->upload->display_errors()
+                    ]);
+                }
+
+            }
+
+        } else {
+
+            if($status) {
+                renderJson([
+                    'status'=>true,
+                    'info'=>$this->upload->data(), //获取上传信息
+                    'target_path' => RESOURCE_URL.'/' . 'uploads' . '/' . $targetDir . $this->upload->data()['orig_name']
+                ]);
+            }else{
+                renderJson([
+                    'status'=>false,
+                    'info'=>$this->upload->display_errors()
+                ]);
+            }
+
         }
+
 
     }
 
