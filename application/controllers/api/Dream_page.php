305bac1dc353cd475cc20ec2ce0106af34d7243b
caofanghui <caofanghui@vipkid.com.cn>
2018-10-15 19:51:59 +0800

    update-支持post传参，接口获值.

--- application/controllers/api/Dream_page.php
+++ application/controllers/api/Dream_page.php
@@ -12,16 +12,35 @@ class Dream_page extends MY_Controller {
     public function get_data () {
         // 处理传参
         $url = isset($_GET['url']) && $_GET['url'] !== '' ? $_GET['url'] : null; // 必填
+//        $type = isset($_GET['type']) && $_GET['type'] !== '' ? $_GET['type'] : 'get'; // 默认是get
 
-        if ($url) {
-            $data = $this->get($url);
-            renderJson($data);
+        // 取得传入数据
+        $data = file_get_contents("php://input") ? json_decode(file_get_contents("php://input"), true) : null; // post传入数据
+
+        if ($data) {
+            if ($url) {
+                $data = $this->post($url, $data);
+                renderJson($data);
+
+            } else {
+                $out_data = out_format(null, '参数有误', 'fail');
+                renderJson($out_data);
+
+            }
 
         } else {
-            $out_data = out_format(null, '参数有误', 'fail');
-            renderJson($out_data);
+            if ($url) {
+                $data = $this->get($url);
+                renderJson($data);
+
+            } else {
+                $out_data = out_format(null, '参数有误', 'fail');
+                renderJson($out_data);
+
+            }
 
         }
+
 
     }
 
